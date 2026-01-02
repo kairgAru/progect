02880016ccd86153f6b3b4a6096478b38c8fd62a
aru <kairg.aru@gmail.com>
2026-01-02 15:21:31 +0600

    first commit

---
+++ script29.js
@@ -0,0 +1,53 @@
+let alo =29;
+console.log(alo);
+
+alo=40;
+console.log(alo);
+
+// let alo=55;
+// console.log(alo)
+
+const game = 5555;
+console.log(game);
+
+// game =345678;
+// console.log(game);
+
+// 2
+var kl = 'jami'; 
+console.log(kl); 
+
+var kl = 'gan'; 
+console.log(kl); 
+// для более надежного и предсказуемого поведения 
+// рекомендуется использовать let или const
+
+// 3
+let num =22;
+let str = "first";
+let bool = true;
+let doh = null;
+let undefinedValue = undefined;
+let notANumber = NaN;
+
+console.log(num);
+console.log(str);
+console.log(bool);
+console.log(doh);
+console.log(undefinedValue);
+console.log(notANumber);
+
+// 4
+console.log(String(doh)); // Вывод: "null"
+console.log(Number(doh)); // Вывод: 0
+
+console.log(String(undefinedValue)); // Вывод: "undefined"
+console.log(Number(undefinedValue)); 
+// 5
+let number = 45;
+console.log(String(number));
+// 6
+const name = prompt("Введите ваше имя:");
+const age = prompt("Введите ваш возраст:");
+console.log(name);
+console.log("Возраст:", age);

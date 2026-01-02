4ca4daaa0ad464903a834a5fe6ef9840c6ff0c3a
aru <kairg.aru@gmail.com>
2026-01-02 16:22:59 +0600

    first commit

---
+++ script30.js
@@ -0,0 +1,30 @@
+let a=10;
+let b=5;
+let c=2;
+
+console.log(a+b+c);
+console.log((a+c)-b);
+console.log((a*b)/c);
+console.log(a%b);
+// 2
+let x =7;
+console.log( x++ + ++x);
+console.log( x-- - --x);
+// 3
+
+let sum = ("The result is: " + (5 + 3)); 
+console.log( String(sum));
+// 4
+console.log( a > b && a > 0 && b > 0 );
+console.log(c<10 ||c==2 );
+console.log(a/b !=5);
+// 5
+let d=3;
+let f='3';
+console.log(d==f);
+console.log(d===f);
+// 6
+let n=9;
+console.log((n % 2 == 0 || n % 3 == 0) && n % 6 !== 0 )
+y=7;
+console.log(y>10 || y<5 )
